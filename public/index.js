355cc4b4f69ac8a2ba5f190057e122bf63b84f6b
Jonathan G. Bogg <jonathan.bogg@gmail.com>
2020-04-27 17:29:27 +0200

    client can create custom open room

--- public/index.js
+++ public/index.js
@@ -1,74 +1,123 @@
 const socket = io()
 
+class Room {
+    constructor(id, usersOnline, isOpen) {
+        this.id = id;
+        this.usersOnline = [usersOnline];
+        this.isOpen = isOpen;
+    }
+}
+
 window.addEventListener('load', () => {
     setupEventListeners()
     roomChosen = "";
 
     let roomChoice = document.getElementsByClassName("roomChoice");
     for (let i = 0; i < roomChoice.length; i++) {
-       roomChoice[i].addEventListener("click", () => {
+        roomChoice[i].addEventListener("click", () => {
 
-        for (let y = 0; y < roomChoice.length; y++) {
-            roomChoice[y].style.backgroundColor = "rgb(55, 209, 183)"
-        }
+            for (let y = 0; y < roomChoice.length; y++) {
+                roomChoice[y].style.backgroundColor = "rgb(55, 209, 183)"
+            }
 
-        roomChosen = roomChoice[i].innerHTML;
-        roomChoice[i].style.backgroundColor = "red"
+            roomChosen = roomChoice[i].innerHTML;
+            roomChoice[i].style.backgroundColor = "red"
 
-       })
+        })
     }
 
 })
 
 function setupEventListeners() {
     // Join submit handler
-    const joinForm = document.querySelector('form.join.ui')
+    const joinForm = document.querySelector('form.join-existing-room')
     joinForm.addEventListener('submit', onJoinRoom)
-    
-    // Send message submit hander
-    const messageForm = document.querySelector('.chat.ui form')
-    messageForm.addEventListener('submit', onSendMessage)
+
+    // Send message when enter is pressed in input field
+    const messageInput = document.querySelector('input.message-input')
+    messageInput.addEventListener('keyup', onSendMessage)
+
+    // Send message when send button is clicked or enter is pressed when button in focus
+    const sendMessageButton = document.querySelector('button.message-input')
+    sendMessageButton.addEventListener('click', onSendMessage)
+    sendMessageButton.addEventListener('keyup', onSendMessage)
+
+    // Show create room form when create room button on front page is clicked
+    const createRoom = document.querySelector('button.create-room')
+    createRoom.addEventListener('click', onCreateRoom)
+
+    // Create and add room when create room form is submitted
+    const createRoomForm = document.querySelector('.join-room>.create-room')
+    createRoomForm.addEventListener('submit', onJoinNewRoom)
 
     // Socket io events
     socket.on('join successful', loadChatUI)
-    socket.on('message', onMessageReceived) 
+    socket.on('update chat', updateChat)
 }
 
 function onJoinRoom(event) {
     event.preventDefault()
 
-    const nameInput = document.querySelector(".join.ui input");
-    
-    const name = nameInput.value
+    const username = document.querySelector(".username-input").value
     const room = roomChosen;
 
-    if(room == "" || room == null) {
+    if (room == "" || room == null) {
         alert("Please choose a room")
     }
     else {
-        socket.emit('join room', { name, room })
+        socket.emit('join room', { username, room })
     }
 
 }
 
 function onSendMessage(event) {
     event.preventDefault()
-    const input = document.querySelector('.chat.ui form input')
-    socket.emit("message", input.value)
-    input.value = ""
+    const messageInput = document.querySelector('input.message-input')
+    const message = messageInput.value
+    if (event.type === 'keyup' && event.keyCode === 13 || event.type === 'click') {
+        if (message === "") {
+            return;
+            // TODO: add error message if message is empty
+        } else {
+            socket.emit('message', message)
+            messageInput.value = ''
+        }
+    }
 }
 
+function createLockedRoom() {
+    
+}
+
+function updateChat({ username, message }) {
+    const ul = document.getElementById("theMessageBoard")
+    const li = document.createElement('li')
+    li.innerText = `${username}: ${message}`
+    ul.append(li)
+}
+
+function onCreateRoom(event) {
+    event.preventDefault()
+    document.querySelector('form.create-room').classList.remove('hidden')
+    document.querySelectorAll('.join-existing-room, button.create-room').forEach(element => element.classList.add('hidden'))
+}
+
+function onJoinNewRoom(event) {
+    event.preventDefault()
+    const username = document.querySelector(".username-input").value
+    const roomName = document.querySelector('.room-name-input').value
+    const room = new Room(roomName, username)
+
+    socket.emit('join room', { username, room })
+
+}
 
 function loadChatUI(data) {
-    console.log(data)
     document.querySelector(".chat.ui").classList.remove("hidden")
     document.querySelector(".join.ui").classList.add("hidden")
 }
 
-function onMessageReceived({ name, message }) {
-    const ul = document.getElementById("theMessageBoard")
-    const li = document.createElement('li')
+const onMessageReceived = ({ username, message }) => {
+    console.log("onmessagerecieved", "name:", username, "msg:", message)
 
-    li.innerText = `${name}: ${message}`
-    ul.append(li)
 }
--- server.js
+++ server.js
@@ -6,30 +6,48 @@ const app = express()
 const server = http.createServer(app)
 const io = socketIO(server)
 
+const users = [
+    { id: 'ted', room: 'room1' },
+    { id: 'angela', room: 'froom' }
+];
+
+const rooms = [
+    { id: 'room1', usersOnline: ['ted'], isLocked: true, password: 'poo' },
+    { id: 'froom', usersOnline: ['angela'], isLocked: false },
+];
 
 app.use(express.static('public'))
 
 io.on('connection', (socket) => {
     console.log('Client connected: ', socket.id)
-
     socket.on('join room', (data) => {
-        socket.join(data.room, () => {
-            // Respond to client that join was successful
-            io.to(socket.id).emit('join successful', 'success')
-
-            // Tell everyone that user has joined room
-            io.to(data.room).emit(
-                'message',
-                {
-                    name: data.name,
-                    message: `Has joined the room!`
-                }
-            )
-        })
+        if (!data.password) {
+            socket.join(data.room.id, () => {
+                // Respond to client that join was successful
+                io.to(socket.id).emit('join successful', 'success')
+                // Clients username saved in socket
+                socket.username = data.username;
+                // Tell everyone that user has joined room
+                io.to(data.room.id).emit(
+                    'update chat', {
+                    username: socket.username,
+                    message: `${rooms.includes(data.room.id) ? 'Joined the room!' : `Created the room "${data.room.id}"`}`
+                })
+                // Add new open room to list of rooms if not already included
+                rooms.includes(data.room.id) ? null : rooms.push(data.room)
+            })
+        } else {
+            if (rooms.includes(data.room.id)) {
+                const foundIndex = rooms.findIndex(data.room.id)
+                if (data.password === rooms[foundIndex].password) {
+                  socket.join  
+                } 
+            }
+        }
 
         socket.on('message', (message) => {
-            // Send Message to everyone in room
-            io.to(data.room).emit('message', { name: data.name, message })
+            // Broadcast message to all clients in the room
+            io.to(data.room.id).emit('update chat', { username: socket.username, message })
         })
     })
 })
