535469a0ba846f94015a6bc91a1544eaba227ac3
Jonathan G. Bogg <jonathan.bogg@gmail.com>
2020-04-28 10:12:25 +0200

    add status-codes.js and locked room dom elements

---
+++ public/status-codes.js
@@ -0,0 +1,8 @@
+const statusCode = {
+    ERROR: {
+        WRONG_PASSWORD: 'WRONG_PASSWORD',
+        ROOM_EXISTS: 'ROOM_EXISTS',
+        SHORT_PASSWORD: 'SHORT_PASSWORD'
+    },
+    SUCCESS: 'SUCCESS'
+}
